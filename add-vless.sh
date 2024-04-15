a309fe8be3d14a0ee5f3a0208e429d6ea819374f
musa3x <166790284+musa3x@users.noreply.github.com>
2024-04-15 17:25:34 +0800

    Update add-vless.sh

--- add-vless.sh
+++ add-vless.sh
@@ -43,8 +43,8 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/test"
-export Server1_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/limit"
+export Server_URL="raw.githubusercontent.com/Musa3x/sshwstrvray/main/test"
+export Server1_URL="raw.githubusercontent.com/Musa3x/sshwstrvray/main/limit"
 export Server_Port="443"
 export Server_IP="underfined"
 export Script_Mode="Stable"
@@ -103,10 +103,10 @@ sed -i '/#vlessgrpc$/a\#vlsg '"$user $exp"'\
 cat > /home/vps/public_html/vless-$user.txt <<-END
 
 ====================================================================
-             P R O J E C T  O F  N E V E R M O R E S S H
+             P R O J E C T  O F  M S S T O R E  S S H
                        [Freedom Internet]
 ====================================================================
-        https://github.com/NevermoreSSH/Blueblue
+        https://github.com/Musa3x/sshwstrvray
 ====================================================================
               Format Vless WS/GRPC
 ====================================================================
