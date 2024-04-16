cecc2f8f35c7bf05f7c3c8d6b6857378c6947474
musa3x <166790284+musa3x@users.noreply.github.com>
2024-04-16 10:43:06 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition V1.0
-# Auther  : NevermoreSSH
+# Auther  : MS STORE
 # (C) Copyright 2022
 # =========================================
 
@@ -19,7 +19,7 @@ IP=$(curl -sS ifconfig.me )
 date=$(date +"%Y-%m-%d-%H:%M:%S")
 domain=$(cat /etc/xray/domain)
 clear
-echo " VPS Data Backup By NevermoreSSH "
+echo " VPS Data Backup By MS STORE SSH "
 sleep 1
 echo -e "[ ${green}INFO${NC} ] Processing . . . "
 mkdir -p /root/backup
@@ -40,14 +40,13 @@ cp -r /etc/gshadow /root/backup/gshadow >/dev/null 2>&1
 cp -r /etc/passwd /root/backup/passwd >/dev/null 2>&1
 cp -r /etc/group /root/backup/group >/dev/null 2>&1
 cd /root
-zip -r $IP-$date-$domain-blueblue.zip backup > /dev/null 2>&1
-rclone copy /root/$IP-$date-$domain-blueblue.zip dr:backup/
+zip -r $IP-$date-$domain-msstore.zip backup > /dev/null 2>&1
+rclone copy /root/$IP-$date-$domain-msstore.zip dr:backup/
 url=$(rclone link dr:backup/$IP-$date-$domain-blueblue.zip)
 id=(`echo $url | grep '^https' | cut -d'=' -f2`)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
 clear
-echo -e "\033[1;37mVPS Data Backup By NevermoreSSH\033[0m
-\033[1;37mTelegram : https://t.me/todfix667 / @NevermoreSSH\033[0m"
+echo -e "\033[1;37mVPS Data Backup By MS STORE\033[0m
 echo ""
 echo "Please Copy Link Below & Save In Notepad"
 echo ""
@@ -57,5 +56,5 @@ echo -e "\033[1;37m$link\033[0m"
 echo ""
 echo "If you want to restore data, please enter the link above"
 rm -rf /root/backup
-rm -r /root/$IP-$date-$domain-blueblue.zip
+rm -r /root/$IP-$date-$domain-msstore.zip
 echo ""
