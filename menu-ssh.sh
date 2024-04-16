4ce00a7dfcce4f524bee6341883f83e296734aae
musa3x <166790284+musa3x@users.noreply.github.com>
2024-04-16 11:17:28 +0800

    Update menu-ssh.sh

--- menu-ssh.sh
+++ menu-ssh.sh
@@ -413,10 +413,10 @@ menu
 clear
 echo -e "${IGreen} ┌────────────────────── SSH MENU ─────────────────────┐${NC}"
 echo -e ""
-echo -e "    ${BICyan}[${BIWhite}1${BICyan}] Add Account SSH \t  ${BICyan}[${BIWhite}5${BICyan}] Multilogin SSH     "
+echo -e "    ${BICyan}[${BIWhite}1${BICyan}] Add Account SSH   ${BICyan}[${BIWhite}5${BICyan}] Multilogin SSH     "
 echo -e "    ${BICyan}[${BIWhite}2${BICyan}] Delete Account SSH  ${BICyan}[${BIWhite}6${BICyan}] Auto Delete user Exp"
 echo -e "    ${BICyan}[${BIWhite}3${BICyan}] Renew Account SSH   ${BICyan}[${BIWhite}7${BICyan}] Auto Kill user SSH"
-echo -e "    ${BICyan}[${BIWhite}4${BICyan}] Check User SSH \t  ${BICyan}[${BIWhite}8${BICyan}] Check Member SSH"
+echo -e "    ${BICyan}[${BIWhite}4${BICyan}] Check User SSH \t   ${BICyan}[${BIWhite}8${BICyan}] Check Member SSH"
 echo -e " ${IGreen}└─────────────────────────────────────────────────────┘${NC}"
 echo -e "     ${BIYellow}Tekan x atau [ Ctrl+C ] • ${BIWhite}untuk Keluar${NC}"
 echo -e "     ${BIBlue}Tekan Enter untuk kembali ke • ${BIWhite}menu utama ${NC}"
