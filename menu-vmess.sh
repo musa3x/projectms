d20942cf5217aa1acfe1b70a4bbb2e2aa4e67b15
musa3x <166790284+musa3x@users.noreply.github.com>
2024-04-16 10:36:22 +0800

    Update menu-vmess.sh

--- menu-vmess.sh
+++ menu-vmess.sh
@@ -210,14 +210,13 @@ echo "Script Mod By NevermoreSSH"
     menu
 }
 clear
-echo -e "${BIGreen} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "       ${BIWhite}${UWhite}VMESS${NC}"
+echo -e "${IYellow} ┌──────────────────── VMESS MENU ─────────────────────┐${NC}"
 echo -e ""
 echo -e "     ${BICyan}[${BIWhite}1${BICyan}] Add Account Vmess     "
 echo -e "     ${BICyan}[${BIWhite}2${BICyan}] Delete Account Vmess     "
 echo -e "     ${BICyan}[${BIWhite}3${BICyan}] Renew Account Vmess     "
 echo -e "     ${BICyan}[${BIWhite}4${BICyan}] Check User XRAY     "
-echo -e " ${BIGreen}└─────────────────────────────────────────────────────┘${NC}"
+echo -e " ${IYellow}└─────────────────────────────────────────────────────┘${NC}"
 echo -e "     ${BIYellow}Tekan x atau [ Ctrl+C ] • ${BIWhite}untuk Keluar${NC}"
 echo -e "     ${BIBlue}Tekan Enter untuk kembali ke • ${BIWhite}menu utama ${NC}"
 echo ""
