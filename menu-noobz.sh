a63bb2ed075c78c1f0012731f1c702ef5fe3bdc6
musa3x <166790284+musa3x@users.noreply.github.com>
2024-05-09 10:21:03 +0800

    Update menu-noobz.sh

--- menu-noobz.sh
+++ menu-noobz.sh
@@ -115,7 +115,7 @@ function member(){
     echo -e "\033[0;34m-------------------------------------------\033[0m" 
    # noobzvpns --info-all-user
    noobzvpns --info-all-user > /tmp/noobuser.txt
-   awk '/^\+/ { user=$2 } /-expired/ { printf "  %-10s\t\t%s\n", user, $2 }' /tmp/noobuser.txt | lolcat
+   awk '/^\+/ { user=$2; sta=$4; }/-expired/ {act=$2; printf " %-10s\t%s\t\t%s\n", user, act, sta }' /tmp/noobuser.txt | lolcat
 
 read -n 1 -s -r -p "Press any key to back on menu"
 menu-noobz
