9a515c6ed0146296c07b0a3b69c6d9dc6a23d868
musa3x <166790284+musa3x@users.noreply.github.com>
2024-05-09 11:06:57 +0800

    Update menuedit.sh

--- menuedit.sh
+++ menuedit.sh
@@ -227,7 +227,7 @@ echo -e "${BICyan} └───────────────────�
 echo -e "${BIBlue} ┌»»»»»»»»»»»»»»»»»»»»»»${BIGreen} Status Service ${NC}${BIBlue}««««««««««««««««««««««««┐"
 #echo -e "| \t\t\t\t\t\t\t\t|"
 echo -e "${BIBlue} |      ${BICyan} SSH ${NC}: $ressh "" ${BICyan} NGINX ${NC}: $resngx "" ${BICyan}  XRAY ${NC}: $resv2r "" ${BICyan} TROJAN ${NC}: $resv2r ${BIBlue}|"
-echo -e "${BIBlue} |  ${BICyan} DROPBEAR ${NC}: $resdbr  " "${BICyan} SSH-WS ${NC}: $ressshws" "${BICyan}NoobVpn ${NC}: $noob  " ${BICyan}Stunnel ${NC}: $resst "\t${BIBlue}|"
+echo -e "${BIBlue} | ${BICyan} DROPBEAR ${NC}: $resdbr  " "${BICyan} SSH-WS ${NC}: $ressshws " "${BICyan} NoobzVpn ${NC}: $noob  " ${BICyan}Stunnel ${NC}: $resst "\t${BIBlue}|"
 echo -e "${BIBlue} └«««««««««««««««««««««««««««««««««»»»»»»»»»»»»»»»»»»»»»»»»»»»»»┘"
 #echo -e " "
 echo -e "${BIGreen} ┌──────────────────────── Menu SSH/XRAY ───────────────────────┐${NC}"
